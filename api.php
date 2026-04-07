16f681bb1708705ca54672930534f49e6e68a1ad
lauraaromano <161627881+lauraaromano@users.noreply.github.com>
2026-04-07 16:12:13 -0300

    Add files via upload

--- api.php
+++ api.php
@@ -1,14 +1,31 @@
 <?php
-//CABECALHO
+    //CABECALHO
     header("Content-Type: application/json"); //Define o tipo de resposta
-    
-    //CONTEUDO
-     $usuarios = [
-          ["id" => 1, "nome" => "Maria Souza", "email" => "maria@email.com"],
-          ["id" => 2, "nome" => "João Silva", "email" => "joao@email.com"]
-    ];
 
-    //Converte para JSON e retorna
-    echo json_encode($usuarios);
+    $metodo = $_SERVER['REQUEST_METHOD'];
+
+    //echo "Método de requesição: ".$metodo;
+
+    switch ($metodo) {
+        case 'GET':
+            echo "AQUI AÇÕES DO MÉTODO GET";
+            break;
+        case 'POST':
+            echo "AQUI AÇÕES DO MÉTODO POST";
+            break;
+        
+        default:
+            echo "MÉTODO NÃO ENCONTRADO";
+            break;
+    }
+
+    // //CONTEUDO
+    //  $usuarios = [
+    //       ["id" => 1, "nome" => "Maria Souza", "email" => "maria@email.com"],
+    //       ["id" => 2, "nome" => "João Silva", "email" => "joao@email.com"]
+    // ];
+
+    // //Converte para JSON e retorna
+    // echo json_encode($usuarios);
     
 ?>
